6777e9dcc76372a0c813796d42b7572f7caefca2
Yoshiki Do <yoshiki.ce.1224@gmail.com>
2026-09-23 15:28:34 -0700

    Adjust home page UI and save slot template

--- static/js/home.js
+++ static/js/home.js
@@ -5,18 +5,31 @@ async function loadSaves() {
         return;
     }
 
-    for (let i = 1; i <= 3; i++) {
-        const slot = document.getElementById(`save-${i}`);
+    const container = document.getElementById("save-slots");
+    const template = document.getElementById("save-slot-template");
 
-        if(slot === null) {
-            continue;
-        }
+    container.innerHTML = "";
+
+    for (let i = 1; i <= 3; i++) {
+        const slot = template.content.firstElementChild.cloneNode(true);
+
+        slot.id = `save-${i}`;
+        slot.querySelector("h2").textContent =`Save ${i}`;
+        
+        container.appendChild(slot);
 
         const farm = data.find(farm => farm.id === i);
 
         if(farm) {
-            slot.querySelector(".save-day").textContent = `Day ${farm.day}`;
-            slot.querySelector(".save-money").textContent = `$${Number(farm.money).toFixed(2)}`;
+            slot.querySelector(".save-year").textContent = `Year: ${farm.year}`;
+
+            const seasonDay = slot.querySelector(".save-season-day");
+            seasonDay.innerHTML = `
+                <img class="save-season-image" src="/static/images/seasons/${farm.season}.png" alt="Season">
+                Day ${farm.season_day}
+            `;
+            
+            slot.querySelector(".save-money").textContent = `Money: $${Number(farm.money).toFixed(2)}`;
 
             const buttons = slot.querySelectorAll("button");
 
@@ -27,7 +40,8 @@ async function loadSaves() {
             buttons[1].onclick = () => deleteGame(farm.id);
 
         } else {
-            slot.querySelector(".save-day").textContent = "Empty";
+            slot.querySelector(".save-year").textContent = "Empty";
+            slot.querySelector(".save-season-day").textContent = "";
             slot.querySelector(".save-money").textContent = "";
 
             const buttons = slot.querySelectorAll("button");
